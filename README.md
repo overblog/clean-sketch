92b304c0a0155659fd585c3bd2ba4fcd214194c6
Florent Cailhol <florent.cailhol@gmail.com>
2014-09-09 16:03:31 +0200

    Add library

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-clean-sketch
-============
+# clean-sketch
+
+> Cleanup [Sketch](http://bohemiancoding.com/sketch/) SVG files
+
+## Install
+
+```sh
+$ npm install --save-dev clean-sketch
+```
+
+## Usage
+
+```js
+var cleanSketch = require('clean-sketch');
+var fs = require('fs');
+
+// Cleanup a Sketch exported SVG
+var svg = cleanSketch(fs.readFileSync('icon.svg', 'utf-8'));
+```
+
+## API
+
+### cleanSketch(doc)
+
+Removes Sketch namespaces and metadata.
+
+#### doc
+
+Type: `String`
+
+## License
+
+MIT © Overblog
---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Removes Sketch namespaces and metadata.
+ *
+ * @param  {String} doc SVG document
+ * @return {String}
+ */
+module.exports = function(doc) {
+  var exprs = [
+    // Comments
+    /<!--.*-->/g,
+    // Namespaces
+    ns('sketch'),
+    ns('xmlns'),
+    ns('xml'),
+    // Optional tags
+    tag('metadata'),
+    tag('title'),
+    tag('desc'),
+    tag('defs'),
+    // Invalid fill
+    attr('fill'),
+  ];
+
+  for (var i = 0; i < exprs.length; i++) {
+    doc = doc.replace(exprs[i], '');
+  }
+
+  return doc;
+};
+
+function tag(name) {
+  return RegExp('<' + name + '>(.|[\n\r\s])*</' + name + '>', 'gi');
+}
+
+function attr(name) {
+  return RegExp(name + '="[^"]*"', 'gi');
+}
+
+function ns(name) {
+  return RegExp(name + ':[^=]+="[^"]*"', 'gi');
+}
